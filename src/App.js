70222bb0aa9682fe0ae32f38302a34a080af3059
Nishant <nishantsurve8484@gmail.com>
2025-03-17 15:34:54 +0530

    updated the App.js

--- src/App.js
+++ src/App.js
@@ -13,8 +13,8 @@ const App = () => {
     <div>
       <Navbar />
       <HeroSection />
-      <Features />
       <CampaignList/>
+      <Features />
       <CampaignManager />
       <Reviews />
       <FAQ />
